be36885109f54374fff7aff5ee3b50205fdafb03
Radhey Chitroda <radheychitroda@gmail.com>
2023-10-15 15:18:12 +0530

    route changes

--- app/api/all/route.ts
+++ app/api/all/route.ts
@@ -2,11 +2,7 @@ import { connectToDb } from "@/utils/database";
 import Task from "@/models/task";
 
 export const GET = async (req: any, res: any) => {
-  res.setHeader(
-    "Cache-Control",
-    "no-cache, no-store, max-age=0, must-revalidate"
-  );
   await connectToDb();
   const tasks = await Task.find({});
-  return new Response(JSON.stringify(tasks), { status: 200 });
+  return new Response(JSON.stringify(tasks), { status: 200, "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate" });
 };
