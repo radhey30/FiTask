3c9f433e1f28e6abb834a29f768ad230bef296a0
Radhey Chitroda <radheychitroda@gmail.com>
2023-10-15 15:10:22 +0530

    cache control

--- app/api/all/route.ts
+++ app/api/all/route.ts
@@ -1,8 +1,12 @@
 import { connectToDb } from "@/utils/database";
 import Task from "@/models/task";
 
-export const GET = async (req:any, res:any) => {
-    await connectToDb()
-    const tasks = await Task.find({})
-    return new Response(JSON.stringify(tasks), {status: 200})
-}
+export const GET = async (req: any, res: any) => {
+  res.setHeader(
+    "Cache-Control",
+    "no-cache, no-store, max-age=0, must-revalidate"
+  );
+  await connectToDb();
+  const tasks = await Task.find({});
+  return new Response(JSON.stringify(tasks), { status: 200 });
+};
