eb4be1113edd9ea4b3404d398e246a0f8d65ab96
Radhey Chitroda <radheychitroda@gmail.com>
2023-10-15 15:20:33 +0530

    route changes

--- app/api/all/route.ts
+++ app/api/all/route.ts
@@ -4,5 +4,5 @@ import Task from "@/models/task";
 export const GET = async (req: any, res: any) => {
   await connectToDb();
   const tasks = await Task.find({});
-  return new Response(JSON.stringify(tasks), { status: 200, "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate" });
+  return new Response(JSON.stringify(tasks), { status: 200 });
 };
